05b607fe756161d0bc938d1c80675714a33799c7
Baris Algun <76767449+berkaybarisalgun@users.noreply.github.com>
2024-07-19 23:31:39 +0300

    frontend Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import './assets/global.css'
+import { Home } from './pages/Home';
 
 export const App = () => {
   return (
-    <div>
-      Hellos
-    </div>
+    <Home></Home>
   )
 }
